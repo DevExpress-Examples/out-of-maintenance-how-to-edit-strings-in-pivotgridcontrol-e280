00c81af8cae68d2e84fc2f8e5208aa1dc2dcebb8
ivan.kozhenov <ivan.kozhenov@devexpress.com>
2022-05-17 18:12:33 +0400

    Migration to Optimized mode
    Migration to Optimized mode and new Data Bindings

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -23,63 +23,70 @@
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
+            Dim DataSourceColumnBinding1 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim DataSourceColumnBinding2 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
             Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
             Me.fieldRow = New DevExpress.XtraPivotGrid.PivotGridField()
             Me.fieldData = New DevExpress.XtraPivotGrid.PivotGridField()
             Me.repositoryItemTextEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemTextEdit()
-            DirectCast(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.repositoryItemTextEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.repositoryItemTextEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
-            ' 
-            ' pivotGridControl1
-            ' 
-            Me.pivotGridControl1.Anchor = (CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+            '
+            'pivotGridControl1
+            '
+            Me.pivotGridControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
             Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.Default
-            Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() { Me.fieldRow, Me.fieldData})
+            Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() {Me.fieldRow, Me.fieldData})
             Me.pivotGridControl1.Location = New System.Drawing.Point(13, 13)
             Me.pivotGridControl1.Name = "pivotGridControl1"
-            Me.pivotGridControl1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() { Me.repositoryItemTextEdit1})
+            Me.pivotGridControl1.OptionsData.DataProcessingEngine = DevExpress.XtraPivotGrid.PivotDataProcessingEngine.Optimized
+            Me.pivotGridControl1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.repositoryItemTextEdit1})
             Me.pivotGridControl1.Size = New System.Drawing.Size(486, 390)
             Me.pivotGridControl1.TabIndex = 0
-            ' 
-            ' fieldRow
-            ' 
+            '
+            'fieldRow
+            '
             Me.fieldRow.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
             Me.fieldRow.AreaIndex = 0
-            Me.fieldRow.FieldName = "row"
+            DataSourceColumnBinding1.ColumnName = "row"
+            Me.fieldRow.DataBinding = DataSourceColumnBinding1
             Me.fieldRow.Name = "fieldRow"
-            ' 
-            ' fieldData
-            ' 
+            '
+            'fieldData
+            '
             Me.fieldData.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
             Me.fieldData.AreaIndex = 0
+            DataSourceColumnBinding2.ColumnName = "data"
+            Me.fieldData.DataBinding = DataSourceColumnBinding2
             Me.fieldData.FieldEdit = Me.repositoryItemTextEdit1
-            Me.fieldData.FieldName = "data"
             Me.fieldData.Name = "fieldData"
             Me.fieldData.Options.ShowGrandTotal = False
             Me.fieldData.SummaryType = DevExpress.Data.PivotGrid.PivotSummaryType.Min
-            ' 
-            ' repositoryItemTextEdit1
-            ' 
+            '
+            'repositoryItemTextEdit1
+            '
             Me.repositoryItemTextEdit1.AutoHeight = False
             Me.repositoryItemTextEdit1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
             Me.repositoryItemTextEdit1.Name = "repositoryItemTextEdit1"
-            ' 
-            ' Form1
-            ' 
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            '
+            'Form1
+            '
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(511, 415)
             Me.Controls.Add(Me.pivotGridControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.repositoryItemTextEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.repositoryItemTextEdit1, System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
 
         End Sub
 
-        #End Region
+#End Region
 
         Private WithEvents pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
         Private fieldRow As DevExpress.XtraPivotGrid.PivotGridField
